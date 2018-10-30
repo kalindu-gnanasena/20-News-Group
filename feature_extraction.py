68a4e078f2700e8de5ed2c62b7920395c941439b
Kumara Fernando <kumara.fernando1995@gmail.com>
2018-10-30 20:40:08 +0530

    feature_extraction.py updated

--- feature_extraction.py
+++ feature_extraction.py
@@ -31,6 +31,7 @@ lemmatizer = WordNetLemmatizer()
 # word list used in english language
 _word_list = set([word for word in wn.words(lang='eng')])
 
+
 # method to get all the text files from the directory to a list
 def read_files(dir):
     f = []
@@ -39,13 +40,13 @@ def read_files(dir):
             f.append(os.path.join(roots, file))
     return f
 
+
 # method to categorize and read the files
 def open_files(file_paths):
     comp = []
     rec = []
     sci = []
     talk = []
-    other = []
     
     for path in file_paths:
         if "comp" in path:
@@ -60,10 +61,21 @@ def open_files(file_paths):
         elif "talk" in path:
             w = open(path, encoding='utf-8', errors='ignore')
             talk += [w.read()]
-        elif "alt" in path or "misc" in path or "soc" in path:
-            w = open(path, encoding='utf-8', errors='ignore')
-            other += [w.read()]
-    return comp, rec, sci, talk, other
+    return comp, rec, sci, talk
+
+
+def get_unique_word_set(data_list):
+    word_set = []
+    for docs in data_list:
+        for doc in docs:
+            words = []
+            for word in word_tokenize(doc):
+                word = stemmer.stem(word)
+                word = lemmatizer.lemmatize(word)
+                words += [word]
+            word_set += [word for word in words if word not in _stop_words if word in _word_list if word.isalpha()]
+    return list(set(word_set))
+
 
 # method to clean data
 def clean_data(data):
@@ -77,80 +89,96 @@ def clean_data(data):
         cleaned_data += [detokenizer.detokenize([word for word in words if word in _word_list if word.isalpha()])]
     return cleaned_data
 
-# frequency vectorizer
-def count_vectorize_csv(cleaned_data, name):
-    vectorizer = CountVectorizer(stop_words=_stop_words)
+
+def count_vectorize_csv(cleaned_data, name, vocabulary_set):
+    vectorizer = CountVectorizer(stop_words=_stop_words, vocabulary=vocabulary_set)
     x = vectorizer.fit_transform(cleaned_data)
     y = vectorizer.get_feature_names()
     
     df = pd.DataFrame(data=x.toarray(), columns=y)
     
-    train, test = train_test_split(df, train_size=0.7, test_size=0.3, random_state=42)
+    df.to_csv(name+'_freq.csv')
     
-    train.to_csv(name+'_count_train.csv')
-    test.to_csv(name+'_count_test.csv')
-    
-    print('Count vectorize train and test csv for {0} is successfully created!'.format(name))
+    print(name+'_freq.csv successfully created!')
 
-# binary vectorizer
-def binary_vectorize_csv(cleaned_data, name):
-    vectorizer = CountVectorizer(stop_words=_stop_words, binary=True)
+
+def binary_vectorize_csv(cleaned_data, name, vocabulary_set):
+    vectorizer = CountVectorizer(stop_words=_stop_words, binary=True, vocabulary=vocabulary_set)
     x = vectorizer.fit_transform(cleaned_data)
     y = vectorizer.get_feature_names()
     
     df = pd.DataFrame(data=x.toarray(), columns=y)
     
-    train, test = train_test_split(df, train_size=0.7, test_size=0.3, random_state=42)
+    df.to_csv(name+'_binary.csv')
     
-    train.to_csv(name+'_binary_train.csv')
-    test.to_csv(name+'_binary_test.csv')
-    
-    print('Binary vectorize train and test csv for {0} is successfully created!'.format(name))
+    print(name+'_binary.csv successfully created!')
 
-# tfidf vectorizer
-def tfidf_vectorize_csv(cleaned_data, name):
-    vectorizer = TfidfVectorizer(stop_words=_stop_words)
+
+def tfidf_vectorize_csv(cleaned_data, name, vocabulary_set):
+    vectorizer = TfidfVectorizer(stop_words=_stop_words, vocabulary=vocabulary_set)
     x = vectorizer.fit_transform(cleaned_data)
     y = vectorizer.get_feature_names()
     
     df = pd.DataFrame(data=x.toarray(), columns=y)
     
-    train, test = train_test_split(df, train_size=0.7, test_size=0.3, random_state=42)
+    df.to_csv(name+'_tfidf.csv')
     
-    train.to_csv(name+'_tfidf_train.csv')
-    test.to_csv(name+'_tfidf_test.csv')
-    
-    print('Tfidf vectorize train and test csv for {0} is successfully created!'.format(name))
+    print(name+'_tfidf.csv successfully created!')
 
-# calling frequency, binary and tfidf vectorizers
-def get_feature_vectors(data, name):
+
+def get_feature_vectors(data, name, vocabulary_set):
     cleaned_data = clean_data(data)
     
-    count_vectorize_csv(cleaned_data, name)
-    binary_vectorize_csv(cleaned_data, name)
-    tfidf_vectorize_csv(cleaned_data, name)
+    count_vectorize_csv(cleaned_data, name, vocabulary_set)
+    binary_vectorize_csv(cleaned_data, name, vocabulary_set)
+    tfidf_vectorize_csv(cleaned_data, name, vocabulary_set)
 
 
-# Running the programme
 def run_main(file_path):
-
     files = read_files(file_path)
 
-    # define lists to store data
     comp_data = []
     rec_data = []
     sci_data = []
     talk_data = []
-    other_data = []
 
-    # open files
-    comp_data, rec_data, sci_data, talk_data, other_data = open_files(files)
-
-    get_feature_vectors(comp_data, 'comp')
-    get_feature_vectors(rec_data, 'rec')
-    get_feature_vectors(sci_data, 'sci')
-    get_feature_vectors(talk_data, 'talk')
-    get_feature_vectors(other_data, 'other')
+    comp_data, rec_data, sci_data, talk_data = open_files(files)
 
 
+    comp_data_train, comp_data_test = train_test_split(comp_data, train_size=0.7, test_size=0.3, random_state=42)
+    rec_data_train, rec_data_test = train_test_split(rec_data, train_size=0.7, test_size=0.3, random_state=42)
+    sci_data_train, sci_data_test = train_test_split(sci_data, train_size=0.7, test_size=0.3, random_state=42)
+    talk_data_train, talk_data_test = train_test_split(talk_data, train_size=0.7, test_size=0.3, random_state=42)
+
+
+    train_data = [comp_data_train, rec_data_train, sci_data_train, talk_data_train]
+    test_data = [comp_data_test, rec_data_test, sci_data_test, talk_data_test]
+
+
+    unique_word_set = get_unique_word_set(train_data)
+    unique_word_set.sort()
+
+
+    # vectorize comp train and test data
+    get_feature_vectors(comp_data_train, 'comp_train', unique_word_set)
+    get_feature_vectors(comp_data_test, 'comp_test', unique_word_set)
+
+    # vectorize rec train and test data
+    get_feature_vectors(rec_data_train, 'rec_train', unique_word_set)
+    get_feature_vectors(rec_data_test, 'rec_test', unique_word_set)
+
+
+    # vectorize sci train and test data
+    get_feature_vectors(sci_data_train, 'sci_train', unique_word_set)
+    get_feature_vectors(sci_data_test, 'sci_test', unique_word_set)
+
+
+    # vectorize talk train and test data
+    get_feature_vectors(talk_data_train, 'talk_train', unique_word_set)
+    get_feature_vectors(talk_data_test, 'talk_test', unique_word_set)
+
+
+
+
+# execute programme
 run_main('./')
